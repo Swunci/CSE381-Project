3bc6d47c461e9f70a8860e8fa9a4248d8f76af75
Haiphen <wanga225@gmail.com>
2019-11-14 04:32:50 -0500

    fixed bug
    done

--- Magicube/Assets/Scripts/LivesScript.cs
+++ Magicube/Assets/Scripts/LivesScript.cs
@@ -13,6 +13,7 @@ public class LivesScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        lifeValue = 1;
         life = GetComponent<Text>();
     }
 
